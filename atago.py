bfeb61822d0a28d57ef6ff7aaec8839aad9e0aa3
Preben Fjell <Preify@outlook.com>
2022-01-06 01:31:19 +0100

    Update atago.py
    Add dictionary.

--- atago.py
+++ atago.py
@@ -1,6 +1,8 @@
 import discord
 import os
 from discord import user
+from requests.models import Response
+from requests.sessions import PreparedRequest
 from riotwatcher import LolWatcher, ApiError
 from discord.ext import commands
 from decouple import config
@@ -12,27 +14,46 @@ os.environ.get('LEAGUE_API')
 
 #LOL WATCHER STUFF
 lol_watcher = LolWatcher(os.getenv('LEAGUE_API'))
-region1 = 'euw1'
+
+#All regions 
+region = {
+    "EUW": "EUW1",
+    "NA": "NA1",
+    "BR": "BR1",
+    "EUNE": "EUNE",
+    "OCE": "OCE",
+    "LAN": "LA1",
+    "LAS": "LA2",
+    "RU": "RU1",
+    "TR": "TR1",
+    "JP": "JP1",
+    "KR": "KR",
+    "PBE": "PBE",
+}
+
 
 #Prefix
-bot = commands.Bot(command_prefix='%')
+bot = commands.Bot(command_prefix='$')
 
 #Commands
 @bot.event # Log-in check
 async def on_ready():
     print('Logged in as user {0.user}'.format(bot))
 
-@bot.event # Placeholder
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@bot.command(name='lookup') # Command for checking league accounts 
+async def __lookup(ctx, arg1, arg2):
+    try:
+        code = lol_watcher.summoner.by_name(region[arg1.upper()], arg2)
+        lookup_reply = discord.Embed(title=code['name'], description='%a \n' % code['summonerLevel'] + 'Test mode') 
+        await ctx.send(embed = lookup_reply)
+    except KeyError:
+        arg2 = region['EUW']
+        code = lol_watcher.summoner.by_name(region[arg2], arg1)
+        lookup_reply = discord.Embed(title=code['name'], description='%a \n' % code['summonerLevel'] + 'Test mode') 
+        await ctx.send(embed = lookup_reply)
 
-@bot.command() # Command for checking league accounts 
-async def league():
-    pass
-    
-    
 
 
-    
+
+
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
