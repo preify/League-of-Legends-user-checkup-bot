a649d4d03cc424b7eb207ae7d8ab0db26e69e9ad
Preben Fjell <Preify@outlook.com>
2022-01-06 05:48:43 +0100

    Update atago.py
    Added localy hosted icons + online ones

--- atago.py
+++ atago.py
@@ -1,11 +1,11 @@
 import discord
 import os
 from discord import user
-from requests.models import Response
-from requests.sessions import PreparedRequest
 from riotwatcher import LolWatcher, ApiError
 from discord.ext import commands
-from decouple import config
+import io
+import aiohttp
+
 
 
 # ENV TABLE STUFF
@@ -14,6 +14,7 @@ os.environ.get('LEAGUE_API')
 
 #LOL WATCHER STUFF
 lol_watcher = LolWatcher(os.getenv('LEAGUE_API'))
+
 
 #All regions 
 region = {
@@ -41,19 +42,26 @@ async def on_ready():
     print('Logged in as user {0.user}'.format(bot))
 
 @bot.command(name='lookup') # Command for checking league accounts 
-async def __lookup(ctx, arg1, arg2):
+async def __lookup(ctx, arg1, arg2=''):
     try:
         code = lol_watcher.summoner.by_name(region[arg1.upper()], arg2)
         lookup_reply = discord.Embed(title=code['name'], description='%a \n' % code['summonerLevel'] + 'Test mode') 
+        lookup_reply.set_author(name=code['name'])
         await ctx.send(embed = lookup_reply)
     except KeyError:
-        arg2 = region['EUW']
-        code = lol_watcher.summoner.by_name(region[arg2], arg1)
+        arg2 = "EUW1"
+        code = lol_watcher.summoner.by_name(arg2, arg1)
         lookup_reply = discord.Embed(title=code['name'], description='%a \n' % code['summonerLevel'] + 'Test mode') 
         await ctx.send(embed = lookup_reply)
 
+@bot.command(name='icon')
+async def __icon(ctx, arg):
+    await ctx.send('https://ddragon.leagueoflegends.com/cdn/10.18.1/img/profileicon/' + arg + '.png')
 
-
-
-
+@bot.command(name='test')
+async def __test(ctx, arg):
+    file = discord.File(f"C:/Users/Fjell/Documents/bot/media/12.1.1/img/profileicon/{arg}.png")
+    embed = discord.Embed()
+    embed.set_image(url="attachment://{arg}.png")
+    await ctx.send(file=file, embed=embed)
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
