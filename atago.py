75cc69cefc5742bbfa90b72d0f3feb602748dc8c
Preben Fjell <Preify@outlook.com>
2022-01-08 19:59:54 +0100

    Update atago.py

--- atago.py
+++ atago.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from discord import user
+from discord import file
 from riotwatcher import LolWatcher, ApiError
 from discord.ext import commands
 import io
@@ -45,13 +46,14 @@ async def on_ready():
 async def __lookup(ctx, arg1, arg2=''):
     try:
         code = lol_watcher.summoner.by_name(region[arg1.upper()], arg2)
-        lookup_reply = discord.Embed(title=code['name'], description='%a \n' % code['summonerLevel'] + 'Test mode') 
-        lookup_reply.set_author(name=code['name'])
+        lookup_reply = discord.Embed(title=code['name'], description=f"Level {str(code['summonerLevel'])} \nIcon ID {str(code['profileIconId'])}")
+        file = discord.File(f"C:/Users/Fjell/Documents/bot/media/12.1.1/img/profileicon/{str(code['profileIconId'])}.png", filename=str(code['profileIconId']))
+        lookup_reply.set_thumbnail(url=f"attachment://{str(code['profileIconId'])}.png")
         await ctx.send(embed = lookup_reply)
     except KeyError:
         arg2 = "EUW1"
         code = lol_watcher.summoner.by_name(arg2, arg1)
-        lookup_reply = discord.Embed(title=code['name'], description='%a \n' % code['summonerLevel'] + 'Test mode') 
+        lookup_reply = discord.Embed(title=code['name'], description='%a \n' % code['summonerLevel']) 
         await ctx.send(embed = lookup_reply)
 
 @bot.command(name='icon')
@@ -60,8 +62,11 @@ async def __icon(ctx, arg):
 
 @bot.command(name='test')
 async def __test(ctx, arg):
-    file = discord.File(f"C:/Users/Fjell/Documents/bot/media/12.1.1/img/profileicon/{arg}.png")
-    embed = discord.Embed()
-    embed.set_image(url="attachment://{arg}.png")
-    await ctx.send(file=file, embed=embed)
+    try:    
+        file = discord.File(f"C:/Users/Fjell/Documents/bot/media/12.1.1/img/profileicon/{arg}.png")
+        await ctx.send(file=file)
+    except:
+        await ctx.send('No such Profile Icon ID number')
+
+
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
